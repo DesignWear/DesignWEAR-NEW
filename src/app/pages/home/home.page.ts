d0612d1f39833b7b0e02743378399e2f131a6b28
diogorcarvalho <diogorcarvalho@gmail.com>
2024-12-28 14:28:47 -0300

    feat(Página Home):

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-splas-page',
   templateUrl: './home.page.html',
+  styleUrl: './home.page.css',
   standalone: true,
+  imports: [RouterLink],
 })
 export class HomePage implements OnInit {
     ngOnInit(): void {
